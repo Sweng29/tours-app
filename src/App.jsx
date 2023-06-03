e6f52707d78ce45b38993f9c91c43ef6cf4f611b
Kashif Ali <kashifali@ophycare.com>
2023-06-04 00:56:11 +0400

    Count down timer added to handle 15 mintues validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { useEffect } from "react";
 import { useState } from "react";
 
 import "./App.css";
 import Tours from "./components/Tours";
 import Loading from "./components/Loading";
+import CountdownTimer from "./components/CountdownTimer";
+import { useCountdown } from "./hooks/useCountdown";
 
 function App() {
   const url = "https://course-api.com/react-tours-project";
   const [tours, setTours] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setError] = useState(false);
+
+  const THREE_DAYS_IN_MS = 1 * 1 * 15 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
 
   useEffect(() => {
     fetchTours();
@@ -18,12 +23,14 @@ function App() {
 
   const fetchTours = async () => {
     setIsLoading(true);
+    setError(false);
     try {
       const toursResponse = await fetch(url);
       const tours = await toursResponse.json();
       setTours(tours);
     } catch (error) {
       console.log(error);
+      setError(true);
     }
     setIsLoading(false);
   };
@@ -35,6 +42,17 @@ function App() {
 
   if (isLoading) {
     return <Loading />;
+  }
+
+  if (isError) {
+    return (
+      <div className="error">
+        <CountdownTimer
+          targetDate={dateTimeAfterThreeDays}
+          fetchTours={fetchTours}
+        />
+      </div>
+    );
   }
 
   if (tours.length == 0) {
---
+++ src/components/CountdownTimer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import DateTimeDisplay from "./DateTimeDisplay";
+import { useCountdown } from "../hooks/useCountdown";
+
+const ExpiredNotice = ({ fetchTours }) => {
+  return (
+    <div className="expired-notice">
+      <span>Your can refresh Page now</span>
+      <button style={{ marginTop: "2rem" }} onClick={() => fetchTours()}>
+        Refresh
+      </button>
+    </div>
+  );
+};
+
+const ShowCounter = ({ days, hours, minutes, seconds }) => {
+  return (
+    <div className="show-counter">
+      <a
+        href="https://tapasadhikary.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="countdown-link"
+      >
+        <DateTimeDisplay value={days} type={"Days"} isDanger={days <= 3} />
+        <p>:</p>
+        <DateTimeDisplay value={hours} type={"Hours"} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={"Mins"} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={"Seconds"} isDanger={false} />
+      </a>
+    </div>
+  );
+};
+
+const CountdownTimer = ({ targetDate, fetchTours }) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice fetchTours={fetchTours} />;
+  } else {
+    return (
+      <section>
+        <h5>Something went worng, try again later after 15 mintues!</h5>
+        <ShowCounter
+          days={days}
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+        />
+      </section>
+    );
+  }
+};
+
+export default CountdownTimer;
